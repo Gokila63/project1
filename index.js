ad2450cc4e113e8b0706771fd8fdef68ce7acdeb
Gokila63 <vigneshvicky61364@gmail.com>
2024-08-24 12:55:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// console.log("gota");
+
+// function phone(a,b,c)
+// {
+//     console.log("this phone is:"+a)
+//     console.log("this phone is:"+b)
+//     console.log("this phone is:"+c)
+// }
+// phone("oppo","redmi","samsong")
+
+
+// function add (a,b)
+
+// {
+//     console.log(a+b)
+// }
+// add(100,100)
+
+
+
+// var l=10
+// var b=50
+
+// function add()
+// {
+//     console.log(l+b)
+// }
+// add()
